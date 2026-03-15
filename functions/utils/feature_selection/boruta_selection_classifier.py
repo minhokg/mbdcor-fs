7045e4e65a1ba4bf2c3da44dad6d622c35289249
minhokg <mkang@flex-power.energy>
2026-03-15 20:42:54 +0100

    Remove n_jobs=-1 in boruta_selection_classifier.py

--- functions/utils/feature_selection/boruta_selection_classifier.py
+++ functions/utils/feature_selection/boruta_selection_classifier.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 
-def boruta_selection(x: np.ndarray, y: np.ndarray, random_state: int = 42) -> List[int]:
+def boruta_selection_classifier(x: np.ndarray, y: np.ndarray, random_state: int = 42) -> List[int]:
     """
     Boruta selection using a Random Forest classifier.
 
@@ -22,7 +22,7 @@ def boruta_selection(x: np.ndarray, y: np.ndarray, random_state: int = 42) -> Li
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=random_state)
 
     # create a random forest classifier
-    rf = RandomForestClassifier(n_jobs=-1, max_depth=5, random_state=random_state)
+    rf = RandomForestClassifier(max_depth=5, random_state=random_state)
 
     # run boruta feature selection
     boruta = BorutaPy(rf, n_estimators="auto", random_state=random_state)
