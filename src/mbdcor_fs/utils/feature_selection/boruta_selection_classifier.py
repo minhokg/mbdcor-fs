c3aaf914920aaa2fb84fe7005fd12f063ed24143
minhokg <mkang@flex-power.energy>
2026-04-28 12:49:29 +0200

    feat #20: Boruta Selection Classifier change; Fix bug for log loss

--- src/mbdcor_fs/utils/feature_selection/boruta_selection_classifier.py
+++ src/mbdcor_fs/utils/feature_selection/boruta_selection_classifier.py
@@ -5,21 +5,25 @@ from boruta import BorutaPy
 from sklearn.ensemble import RandomForestClassifier
 
 
-def boruta_selection_classifier(x_train: np.ndarray, y_train: np.ndarray, random_state: int = 42) -> List[int]:
+def boruta_selection_classifier(x: np.ndarray, y: np.ndarray, random_state: int = 42) -> List[int]:
     """
     Boruta selection using a Random Forest classifier.
 
-    :param x_train: feature of training set
-    :param y_train: label of training set
-    :param random_state: random seed
-    :return: list of selected features
+    Args:
+        x (np.ndarray): Feature matrix
+        y (np.ndarray): Target vector
+        random_state (int, optional): Random state. Defaults to 42.
+
+    Returns:
+        List[int]:
+
     """
     # create a random forest classifier
-    rf = RandomForestClassifier(random_state=random_state)
+    rf = RandomForestClassifier(max_depth=5, random_state=random_state)
 
     # run boruta feature selection
     boruta = BorutaPy(rf, n_estimators="auto", random_state=random_state)
-    boruta.fit(x_train, y_train)
+    boruta.fit(x, y)
 
     # get selected features
     selected_features_idx = [index for index, value in enumerate(boruta.support_) if value]
